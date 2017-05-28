ec14acbdc4145e822665a4f42d9d7cfed0a72ce9
Carmona <13030617@itcelaya.edu.mx>
2017-05-28 17:37:01 -0500

    Import the the GUI principalFrame and comment it

--- src/se/determinista/DeterministES.java
+++ src/se/determinista/DeterministES.java
@@ -1,11 +1,14 @@
 package se.determinista;
 
 import se.determinista.UI.MainMenu;
+import se.determinista.GUI.PrincipalFrame;
 
 
 public class DeterministES {
 
     public static void main(String[] args) {
         new MainMenu();
+        //PrincipalFrame frame = new PrincipalFrame();
+        //frame.setVisible(true);
     }
 }
